1850eefbeb7bca7c8a5afbace41a296dc304d9da
Roman <romka1@gmail.com>
2019-04-26 19:16:58 +0300

    qwqwqee

---
+++ pitanie/Проедая свой путь к исцелению и совершенству_files/a.js
@@ -0,0 +1,22 @@
+BM_STAT={site:165010, total:1002249, hosts:9, hits:11, users:9, online:3, time: 1556096495};try{;BM_Draw(BM_STAT);}catch(e){}finally{};
+/*
+     FILE ARCHIVED ON 09:01:35 Apr 24, 2019 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 11:19:15 Apr 24, 2019.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 111.746 (3)
+  esindex: 0.011
+  captures_list: 140.708
+  CDXLines.iter: 23.626 (3)
+  PetaboxLoader3.datanode: 84.473 (4)
+  exclusion.robots: 0.286
+  exclusion.robots.policy: 0.267
+  RedisCDXSource: 0.856
+  PetaboxLoader3.resolve: 13.938
+  load_resource: 26.798
+*/
